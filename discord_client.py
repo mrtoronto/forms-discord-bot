eec25298febd6ffd91ed0d1a1bf92eab667c7857
mrtoronto <matt.toronto97@gmail.com>
2023-02-07 08:50:44 -0800

    twitter listener

--- discord_client.py
+++ discord_client.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timedelta
 import json
-import os
+import random
+import tweepy
 import time
 import discord
 from discord.ext import tasks
-from local_settings import DISCORD_TOKEN
+from local_settings import DISCORD_TOKEN, TWITTER_TOKEN
 import logging
 
 from config_parameters import *
@@ -15,6 +16,9 @@ lock = FileLock("data/forms_points.json.lock")
 
 logger = logging.getLogger('FORMS_BOT')
 
+embed_colors = [0xA429D6, 0xB654DE]
+
+
 class FormsClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -23,6 +27,15 @@ class FormsClient(discord.Client):
         self.pending_alpha = {}
         self.all_sent_alpha = []
 
+        self.past_tweet_ids = []
+
+        self.next_token = None
+
+        self.client = tweepy.Client(
+            TWITTER_TOKEN,
+            return_type=dict
+        )
+
     def _load_forms_points(self):
         with lock:
             with open('data/forms_points.json', 'r') as f:
@@ -30,11 +43,13 @@ class FormsClient(discord.Client):
 
     async def setup_hook(self) -> None:
         ### Checks one channel for alpha now. Could be updated to check a list of channels or all
-        self.check_recent_messages_task.start(channel_ids=CHANNELS_TO_CHECK)
-        self.check_all_alpha_messages.start()
+        # self.check_recent_messages_task.start(channel_ids=CHANNELS_TO_CHECK)
+        # self.check_all_alpha_messages.start()
+        self.check_twitter.start()
 
         ### Used when bot sends a message about alpha
         self.ALPHA_CHANNEL = await self.fetch_channel(ALPHA_CHANNEL_ID)
+        self.TWITTER_CHANNEL = await self.fetch_channel(TWITTER_CHANNEL_ID)
 
     async def on_ready(self):
         logger.info(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -51,7 +66,6 @@ class FormsClient(discord.Client):
         ### Check for new messages with alpha reacts
         ### Check messages that had alpha reacts before to see if any broke threshold
         self._load_forms_points()
-
 
     @tasks.loop(seconds=CHECK_RECENT_MESSAGES_INTERVAL)  # task runs every 60 seconds
     async def check_recent_messages_task(self, channel_ids, threshold=LAYER_1_ALPHA_THRESHOLD):
@@ -124,6 +138,64 @@ class FormsClient(discord.Client):
         await self.wait_until_ready()  # wait until the bot logs in
 
     @check_all_alpha_messages.before_loop
+    async def before_my_task(self):
+        await self.wait_until_ready()  # wait until the bot logs in
+    
+
+    @tasks.loop(seconds=CHECK_TWITTER_INTERVAL)  # task runs every 60 seconds
+    async def check_twitter(self):
+        result = self.client.get_list_tweets(
+            id='7450', expansions=["author_id", "attachments.media_keys"],
+            tweet_fields=['attachments', 'public_metrics', 'created_at', 'author_id'],
+            user_fields=['profile_image_url', 'username'],
+            media_fields=['url', 'preview_image_url'], 
+            max_results=5,
+            pagination_token=self.next_token
+        )
+        print(result)
+        next_token = result['meta']['next_token']
+        tweet_data = result['data']
+        user_data = result['includes']['users']
+
+        try:
+            media_data = result['includes']['media']
+        except:
+            media_data = []
+
+        print('Tweet DATA: ', len(tweet_data), tweet_data[0])
+        print('USERS DATA: ', len(user_data), user_data[0])
+        if media_data:
+            print('MEDIA DATA: ', len(media_data), media_data[0])
+
+        for tweet in tweet_data:
+            if tweet['id'] in self.past_tweet_ids:
+                continue
+
+            author_id = tweet['author_id']
+            if 'attachments' in tweet:
+                try:
+                    media_key = tweet['attachments']['media_keys'][0]
+                    media_tweet = [i for i in media_data if i['media_key'] == media_key][0]
+                except:
+                    media_tweet = {}
+
+            else:
+                media_tweet = {}
+
+            user = [i for i in user_data if i['id'] == author_id][0]
+            print(f'Sending Discord message for {tweet}')
+            embed = discord.Embed(
+                title="Tweet", 
+                url=f"https://twitter.com/{user['username']}/status/{tweet['id']}",
+                description=f"{tweet['text']}", 
+                timestamp=datetime.strptime(tweet['created_at'], "%Y-%m-%dT%H:%M:%S.%fZ"),
+                colour=random.choice(embed_colors)
+            )  # Initializing an
+            await self.TWITTER_CHANNEL.send(
+                embed=embed
+            )
+
+    @check_twitter.before_loop
     async def before_my_task(self):
         await self.wait_until_ready()  # wait until the bot logs in
 
